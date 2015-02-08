a4dcd719c9c33c72e7d318fc5cc857594ec180b3
ivanyukio <ivan.yk14@gmail.com>
2015-02-08 17:36:27 -0500

    fix comments not working

--- app/controllers/comment_routes.rb
+++ app/controllers/comment_routes.rb
@@ -1,10 +1,11 @@
-post '/items/:category_id/comments/:id' do
+post '/categories/:category_id/items/:item_id/comments' do
   p params
-  Comment.create(params)
+  Comment.create(content: params[:content], user_id: session[:user].id, item_id: params[:item_id])
   redirect back
 end
 
-put '/items/:category_id/comments/:id' do
-  Comment.update_attributes(params)
+put '/categories/:category_id/items/:item_id/comments/:comment_id' do
+  p params
+  Comment.update(params[:comment_id], {:content => params[:content]})
   redirect back
 end
